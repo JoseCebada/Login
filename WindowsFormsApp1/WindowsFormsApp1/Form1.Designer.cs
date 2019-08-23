7abd8c41af37f4e602f6058917ab9bec3a8718b6
Alumno <Alumno@PC26>
2019-08-23 12:25:04 -0500

    Final

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,29 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnEntrar = new System.Windows.Forms.Button();
             this.lbInicio = new System.Windows.Forms.Label();
             this.lbUsuario = new System.Windows.Forms.Label();
             this.lbContra = new System.Windows.Forms.Label();
             this.txtContra = new System.Windows.Forms.TextBox();
             this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEntrar
             // 
+            this.btnEntrar.BackColor = System.Drawing.Color.Green;
             this.btnEntrar.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
             this.btnEntrar.Location = new System.Drawing.Point(287, 239);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(113, 36);
+            this.btnEntrar.Size = new System.Drawing.Size(113, 37);
             this.btnEntrar.TabIndex = 0;
             this.btnEntrar.Text = "Entrar";
-            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.UseVisualStyleBackColor = false;
             this.btnEntrar.Click += new System.EventHandler(this.Button1_Click);
             // 
             // lbInicio
             // 
             this.lbInicio.AutoSize = true;
+            this.lbInicio.BackColor = System.Drawing.Color.Transparent;
             this.lbInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInicio.ForeColor = System.Drawing.Color.White;
             this.lbInicio.Location = new System.Drawing.Point(250, 19);
             this.lbInicio.Name = "lbInicio";
             this.lbInicio.Size = new System.Drawing.Size(190, 33);
@@ -60,7 +66,9 @@
             // lbUsuario
             // 
             this.lbUsuario.AutoSize = true;
+            this.lbUsuario.BackColor = System.Drawing.Color.Transparent;
             this.lbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsuario.ForeColor = System.Drawing.Color.White;
             this.lbUsuario.Location = new System.Drawing.Point(196, 111);
             this.lbUsuario.Name = "lbUsuario";
             this.lbUsuario.Size = new System.Drawing.Size(93, 25);
@@ -71,7 +79,9 @@
             // lbContra
             // 
             this.lbContra.AutoSize = true;
+            this.lbContra.BackColor = System.Drawing.Color.Transparent;
             this.lbContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbContra.ForeColor = System.Drawing.Color.White;
             this.lbContra.Location = new System.Drawing.Point(174, 177);
             this.lbContra.Name = "lbContra";
             this.lbContra.Size = new System.Drawing.Size(133, 25);
@@ -95,11 +105,25 @@
             this.txtUsuario.TabIndex = 5;
             this.txtUsuario.TextChanged += new System.EventHandler(this.TxtUsuario_TextChanged);
             // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(565, 19);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(80, 33);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(683, 299);
+            this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.txtContra);
             this.Controls.Add(this.lbContra);
@@ -122,6 +146,7 @@
         private System.Windows.Forms.Label lbContra;
         private System.Windows.Forms.TextBox txtContra;
         private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -56,5 +56,10 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
